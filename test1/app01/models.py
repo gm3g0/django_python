b6094610bda5ca19ce332c24e99763f63a4d5805
Cathy910628 <shaner8138540@gmail.com>
2023-01-31 13:32:03 +0800

    登入註冊

--- test1/app01/models.py
+++ test1/app01/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+##### 建造資料表 #####
+
+
+class UserInfo(models.Model):  # 使用者
+    email = models.CharField(verbose_name="信箱", max_length=100)
+    name = models.CharField(verbose_name="姓名", max_length=32)
+    password = models.CharField(verbose_name="密碼", max_length=64)
+    sex = models.CharField(
+        verbose_name="生理性別", max_length=4, null=True, blank=True)
+    bithday = models.DateField(verbose_name="生日")
+    Depart = models.ForeignKey(verbose_name="科系", to="Department",
+                               to_field="id", null=True, blank=True, on_delete=models.SET_NULL)
+
+
+class Department(models.Model):  # 科系別
+    title = models.CharField(verbose_name="科系", max_length=32)
+
+    def __str__(self):
+        return self.title
--- test1/app01/views.py
+++ test1/app01/views.py
@@ -1,5 +1,10 @@
-from django.shortcuts import render, HttpResponse
-
+from django import forms
+from django.shortcuts import render, HttpResponse, redirect
+from django.core.exceptions import ValidationError
+from app01 import models
+from app01.models import UserInfo
+from app01.utils.encrypt import md5
+from app01.utils.bootstrap import BootStrapForm
 # Create your views here.
 
 
@@ -7,15 +12,91 @@ def index(request):
     return HttpResponse("哈囉")
 
 
-def login(request):
-    return render(request, "login.html")
+class LoginForm(BootStrapForm):  # 登入欄位
+    email = forms.CharField(
+        label="E-mail",
+        widget=forms.TextInput,
+        required=True
+    )
+    password = forms.CharField(
+        label="密碼",
+        widget=forms.PasswordInput,
+        required=True
+    )
+
+    def clean_password(self):
+        pwd = self.cleaned_data.get("password")
+        return md5(pwd)
 
 
-def singup(req):
-    return render(req, "singup.html")
+def login(request):  # 登入
+    ##### 登入 #####
+    if request.method == "GET":
+        form = LoginForm()
+        return render(request, 'login.html', {'form': form})
+
+    form = LoginForm(data=request.POST)
+    if form.is_valid():
+
+        # 成功拿到值 {'name':'test123', 'password': '123'}
+        print(form.cleaned_data)
+
+        # 到資料庫比對資料(**以字典型態)
+        # 登入頁面所填的資料
+        user_object = UserInfo.objects.filter(**form.cleaned_data).first()
+
+        if not user_object:  # 比對失敗
+            form.add_error("password", "名稱或密碼錯誤")  # 在password下方顯示錯誤訊息
+            return render(request, 'login.html', {'form': form})
+
+        # 比對成功
+        # 在 cookie 和 session 加入字串
+        request.session['info'] = {
+            'id': user_object.id, 'name': user_object.name}
+        return redirect("/teaching/")
+
+    return render(request, 'login.html', {'form': form})
+
+
+CHOICES = [('1', '男性'),
+           ('2', '女性')]
+
+
+class UserModelForm(forms.ModelForm):
+    class Meta:
+        model = models.UserInfo
+        fields = ["email", "name", "password", "bithday", "sex", "Depart"]
+
+        widgets = {  # 樣式設定
+            "password": forms.PasswordInput(render_value=True),
+            "bithday": forms.DateInput(attrs={'type': 'date'}),
+            "sex": forms.RadioSelect(choices=CHOICES, attrs={'class': 'form-input'}),
+        }
+
+    def clean_password(self):
+        pwd = self.cleaned_data.get("password")
+        return md5(pwd)
+
+
+def singup(request):  # 註冊
+ ##### ModelForm方式增加使用者 #####
+
+    if request.method == 'GET':
+
+        form = UserModelForm()
+        return render(request, 'singup.html', {"form": form})
+
+    form = UserModelForm(data=request.POST)
+    if form.is_valid():
+        form.save()
+        return redirect("/teaching/")
+
+    return render(request, 'singup.html', {"form": form})
+
 
 def exercise(req):
     return render(req, "exercise.html")
 
+
 def teaching(req):
-    return render(req, "teaching.html")    
+    return render(req, "teaching.html")
